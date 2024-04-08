dc586f4a850cb7534f57870c1bcb1ed16e78b090
b10856039 <ycenz234@gmail.com>
2024-04-08 18:57:11 +0800

    fix cors

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.use(cors())
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //404 Handler
 app.use(function(req, res, next) {
   next(createError(404));
